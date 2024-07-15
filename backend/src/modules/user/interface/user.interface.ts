83b448f0569b90179c78cb2c8c94f072dfce54fb
gabesouto <soutogabriel04@gmail.com>
2024-07-15 20:32:51 -0300

    chore(arch): modules folder structure defined/renamed files

---
+++ backend/src/modules/user/interface/user.interface.ts
@@ -0,0 +1,7 @@
+interface User {
+   name: string,
+   password: string,
+   email: string,
+   role: string,
+   age: number
+}
--- backend/src/modules/user/service/user.service.ts
+++ backend/src/modules/user/service/user.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class UserService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  
+  post(user:User ): User {
+    return user
   }
 }
