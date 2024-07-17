b2593b314e23d1de660ed2190f29ad381deaac12
gabesouto <soutogabriel04@gmail.com>
2024-07-17 19:18:20 -0300

    refactor: modules created for better code organization.

--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common'
-import { UserController } from './modules/user/controller/user.controller'
-import { UserService } from './modules/user/service/user.service'
-import { PrismaService } from './database/prisma.service'
-import { ExcludeService } from './modules/user/helpers/exclude.service'
+import { UsersModule } from './modules/user/user.module'
 
 @Module({
-  imports: [],
-  controllers: [UserController],
-  providers: [UserService, PrismaService, ExcludeService],
+  imports: [UsersModule],
 })
 export class AppModule {}
