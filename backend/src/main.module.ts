078893e929114d9e35f075aacc3079b8ed270434
gabesouto <soutogabriel04@gmail.com>
2024-07-17 12:05:01 -0300

    feat: excludeService added

--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './modules/user/controller/user.controller';
 import { UserService } from './modules/user/service/user.service'
 import { PrismaService } from './database/prisma.service';
+import { ExcludeService } from './modules/user/helpers/exclude.service';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, ExcludeService],
 })
 export class AppModule {}
