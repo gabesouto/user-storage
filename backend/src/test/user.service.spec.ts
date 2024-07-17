470e98e207d87ff52463d5f50995e83310d13383
gabesouto <soutogabriel04@gmail.com>
2024-07-17 10:56:31 -0300

    perf: user service test improved

--- backend/src/test/user.service.spec.ts
+++ backend/src/test/user.service.spec.ts
@@ -1,34 +1,65 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from '../modules/user/controller/user.controller';
-import { UserService } from '../modules/user/service/user.service';
-import { PrismaService } from '../database/prisma.service';
-import { TestBed } from '@automock/jest';
+import { Test, TestingModule } from '@nestjs/testing'
 import { randomUUID } from 'crypto';
-import { getPrismaClient } from '@prisma/client/runtime/library';
-import { PrismaClient } from '@prisma/client';
+import { PrismaService } from "../database/prisma.service"
+import { UserService } from '../modules/user/service/user.service'
+    
 
-describe('UserService', () => {
-  let userService: UserService;
-  let mockPrismaService = {}
+describe('PostsService', () => {
+  let service: UserService;
+  let prisma: PrismaService;
+
+  let mockUser = {
+    id: randomUUID(),
+    fullName: "mock user",
+    email: "mock@gmail.com",
+    age: 24,
+    role: "admin",
+    createdAt: Date.now()
+  }
+
+  let prismaMock = {
+    user: {
+      create: jest.fn().mockReturnValue(mockUser),
+      // findMany: jest.fn().mockResolvedValue(fakePosts),
+      // findUnique: jest.fn().mockResolvedValue(fakePosts[0]),
+      // update: jest.fn().mockResolvedValue(fakePosts[0]),
+      // delete: jest.fn(), // O método delete não retorna nada
+    },
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
-        {
-          provide: PrismaService,
-          useValue: mockPrismaService,
-        },
+        { provide: PrismaService, useValue: prismaMock },
       ],
     }).compile();
 
-    userService = module.get<UserService>(UserService);
+    service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
-  // Add your tests here
-  it('should be defined', () => {
-    expect(userService).toBeDefined()
-  })
-});
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  // TODO: acesso ao id na hora do mock esta quebrando o test
+
+  describe('create', () => {
+    it(`should create a new user`, async () => {
+
+      let newUser = {
+        fullName: "mock user",
+        email: "mock@gmail.com",
+        password: "12345678",
+        age: 24,
+        role: "admin",
+      }
+      
+      const response = await service.post(newUser);
+
+      expect(response).toStrictEqual({data: mockUser})
+      expect(prisma.user.create).toHaveBeenCalledTimes(1);
+
+    });
+  });
+});
