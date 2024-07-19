6967be3ed3f90d46f7af5fc971b62a9eebd692bb
gabesouto <soutogabriel04@gmail.com>
2024-07-19 12:18:18 -0300

    refactor: improved response return, now returns an DTO instead of an Interdface.

--- backend/src/modules/user/dto/user.dto.ts
+++ backend/src/modules/user/dto/user.dto.ts
@@ -7,6 +7,7 @@ import {
   IsInt,
   Min,
   Max,
+  IsDate,
 } from 'class-validator'
 
 export class CreateUserDto {
@@ -87,4 +88,46 @@ export class UpdateUserDto {
     example: 30,
   })
   age: number
+}
+
+export class ResponseUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The full name of the user',
+    example: 'John Doe',
+  })
+  fullName: string
+
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The email address of the user',
+    example: 'john.doe@example.com',
+  })
+  email: string
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The role of the user',
+    example: 'admin',
+  })
+  role: string
+
+  @IsInt()
+  @Min(0)
+  @Max(120)
+  @ApiProperty({
+    description: 'The age of the user. Must be between 0 and 120.',
+    example: 30,
+  })
+  age: number
+
+  @IsDate()
+  @ApiProperty({
+    description: 'The date when the user was created.',
+    example: '2024-07-19T15:30:00.000Z',
+  })
+  createdAt: Date
 }
