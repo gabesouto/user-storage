8b4d8214490fcd6289bf7a999daffb0eab8bd8ab
gabesouto <soutogabriel04@gmail.com>
2024-07-20 16:24:34 -0300

    feat: login initial page added.

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from './login.form'
+
+function Login() {
+  return (
+    <div className="z-1 flex flex-col gap-4 w-full h-full dark:bg-gray-800 absolute top-0 left-0 right-0 bottom-0 items-center justify-center">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default Login
